924cda3749ff5dd0646b92cf1052b0876463ca4e
hey-sk <stephenakeane@gmail.com>
2023-12-27 15:48:28 -0500

    tweaked for better time to execute

--- 2023/day12/part2.py
+++ 2023/day12/part2.py
@@ -1,8 +1,9 @@
 import time
+from functools import cache
 inputPath = "2023/day12/input.txt"
 testPath = "2023/day12/testInput1.txt"
-chosenPath = inputPath
-
+chosenPath = testPath
+part = 'part2'
 
 startTime = time.time()
 
@@ -12,34 +13,50 @@ with open(chosenPath, 'r') as file:
 results = []
 for line in input:
     startingSeq = [int(x) for x in line.split()[1].split(',')]
-    seq = []
     startingSpringRecord = line.split()[0]
-    springRecord = []
-    for i in range(5):
-       seq += startingSeq
-       springRecord += startingSpringRecord + '?'
-    '''
-    seq = [int(x) for x in line.split()[1].split(',')]
-    springRecord = line.split()[0]
-    '''
+    if part == 'part1':
+        seq = [int(x) for x in line.split()[1].split(',')]
+        springRecord = line.split()[0]
+    
+    if part == 'part2':
+        seq = []
+        springRecordArr = []
+        for i in range(5):
+            seq += startingSeq
+            springRecordArr += startingSpringRecord + '?'
+        springRecord = ''.join(springRecordArr)
+    
     # check the string to see if it's valid given the sequence provided.
     def isValid(seq, possibility):
         springResults = [len(x) for x in possibility.split('.') if x != '']
         return(springResults == seq)
-    # generate a list of all of the the different possible replacements for any char == '?'
-    # e.g. '???' could be '###' or '##.' or '#..' etc...
+    
+    # check the partially built string to see if it's valid... 
+    # we should abandon any additional efforts to build out strings that are invalid.
+    def wipIsValid(seq, wip):
+        if wip.__contains__('#') and wip[-1] == '.':
+            wipSequences = [len(x) for x in wip.split('.') if x != '']
+            if len(wipSequences) <= len(seq):
+                for wipIdx, num in enumerate(wipSequences):
+                    if num != seq[wipIdx]:
+                        return False 
+        return True
+
+    # update the string based on the different possible combinations.
     def generateSubstitutions(charCount, charIndex=0, current=[]):
         if charCount == 0:
             if isValid(seq, ''.join(current)):
                 results.append(1)
-            charIndex = 0
-            return
+                return
         elif springRecord[charIndex] != '?':
+            if not wipIsValid(seq, ''.join(current)):
+                return
             generateSubstitutions(charCount - 1, charIndex = charIndex + 1, current = current + [springRecord[charIndex]])
         else:
             for char in ['.','#']:
+                if not wipIsValid(seq, ''.join(current)):
+                    return
                 generateSubstitutions(charCount - 1, charIndex = charIndex + 1, current = current + [char])
-    print(f'reviewing: {line}')
     generateSubstitutions(len(springRecord))
 
     endTime = time.time()
