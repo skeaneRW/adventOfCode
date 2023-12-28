10a6e42d3915e494d1385a40052c7df89e8bb151
hey-sk <stephenakeane@gmail.com>
2023-12-27 22:18:25 -0500

    needed help on part2-- thanks https://www.youtube.com/watch?v=FCub4W709G0

--- 2023/day12/part2.py
+++ 2023/day12/part2.py
@@ -2,7 +2,7 @@ import time
 from functools import cache
 inputPath = "2023/day12/input.txt"
 testPath = "2023/day12/testInput1.txt"
-chosenPath = testPath
+chosenPath = inputPath
 part = 'part2'
 
 startTime = time.time()
@@ -10,60 +10,55 @@ startTime = time.time()
 with open(chosenPath, 'r') as file:
     input = file.readlines()
 
-results = []
-for line in input:
-    startingSeq = [int(x) for x in line.split()[1].split(',')]
-    startingSpringRecord = line.split()[0]
-    if part == 'part1':
-        seq = [int(x) for x in line.split()[1].split(',')]
-        springRecord = line.split()[0]
-    
+total = 0
+def parse(line: str):
+    record, groups = line.split(" ")
+    groups = tuple(map(int, groups.split(",")))
     if part == 'part2':
-        seq = []
-        springRecordArr = []
-        for i in range(5):
-            seq += startingSeq
-            springRecordArr += startingSpringRecord + '?'
-        springRecord = ''.join(springRecordArr)
-    
-    # check the string to see if it's valid given the sequence provided.
-    def isValid(seq, possibility):
-        springResults = [len(x) for x in possibility.split('.') if x != '']
-        return(springResults == seq)
-    
-    # check the partially built string to see if it's valid... 
-    # we should abandon any additional efforts to build out strings that are invalid.
-    def wipIsValid(seq, wip):
-        if wip.__contains__('#') and wip[-1] == '.':
-            wipSequences = [len(x) for x in wip.split('.') if x != '']
-            if len(wipSequences) <= len(seq):
-                for wipIdx, num in enumerate(wipSequences):
-                    if num != seq[wipIdx]:
-                        return False 
-        return True
+        record = "?".join([record] * 5)
+        groups *= 5
+    return record, groups
 
-    # update the string based on the different possible combinations.
-    def generateSubstitutions(charCount, charIndex=0, current=[]):
-        if charCount == 0:
-            if isValid(seq, ''.join(current)):
-                results.append(1)
-                return
-        elif springRecord[charIndex] != '?':
-            if not wipIsValid(seq, ''.join(current)):
-                return
-            generateSubstitutions(charCount - 1, charIndex = charIndex + 1, current = current + [springRecord[charIndex]])
-        else:
-            for char in ['.','#']:
-                if not wipIsValid(seq, ''.join(current)):
-                    return
-                generateSubstitutions(charCount - 1, charIndex = charIndex + 1, current = current + [char])
-    generateSubstitutions(len(springRecord))
+def isValid (record, startPoint, endPoint):
+    # record cannot be out of bounds
+    if startPoint - 1 <  0 or endPoint + 1 >= len(record):
+        return False
+    # record can't have an additional hash at either end
+    if record[startPoint - 1] == '#' or record[endPoint + 1] == '#':
+        return False
+    # we cant exclude prior hashes
+    if '#' in record[:startPoint]:
+        return False
+    # check to make sure we don't have any dots in the target segment
+    for charIndex in range(startPoint, endPoint + 1):
+        if record[charIndex] == '.':
+            return False
+    # if we pass all previous checks, the result is valid
+    print(record, startPoint, endPoint)
+    return True
 
-    endTime = time.time()
-    elapsedTime = round(endTime - startTime, 3)
-
-print(sum(results))
-print(f'time elapsed: {elapsedTime} seconds')
+@cache
+def depthFirstSearch(record, groups):
+    # if we go through the solution and get to the end there should
+    # be no more hash symbols. if you still have a hash, it's not a valid solution.
+    if not groups:
+        return 0 if "#" in record else 1
     
-
+    hashSize, restOfGroup = groups[0], groups[1:]
+    count = 0
     
+    for endPoint in range(len(record)):
+        startPoint = endPoint - (hashSize - 1)
+        if isValid(record, startPoint, endPoint):
+            count += depthFirstSearch(record[endPoint + 1:], restOfGroup)
+    return count
+
+for lineIdx, line in enumerate(input):
+    record, groups = parse(line)
+    record = '.' + record + '.'
+    total += depthFirstSearch(record, groups)
+
+
+print(total)
+    
+   
---
+++ 2023/day13/part2.py
@@ -0,0 +1,5 @@
+inputPath = "2023/day13/input.txt"
+testPath = "2023/day13/testInput1.txt"
+chosenPath = inputPath
+
+    
