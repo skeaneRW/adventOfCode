5a12883d964b8152b7b6fc457088b9ad6bdf9108
hey-sk <stephenakeane@gmail.com>
2023-12-28 12:54:59 -0500

    messing around with day 13-2 still needs some work...

--- 2023/day13/part2.py
+++ 2023/day13/part2.py
@@ -1,6 +1,7 @@
+from functools import cache
 inputPath = "2023/day13/input.txt"
 testPath = "2023/day13/testInput1.txt"
-chosenPath = inputPath
+chosenPath = testPath
 
 def parseInput(path):
     result = []
@@ -17,9 +18,9 @@ def parseInput(path):
     return result
 
 groupings = parseInput(chosenPath)
-
+alternates = []
 # review columns to see if any adjacent columns mirror one another.
-def checkColumns(group):
+def checkColumns(group, setAlternates):
     validColumns = []
     midPoint = len(group[0])/2
     #accumulate any indexes where the left side and the right side (mirrored) match
@@ -42,19 +43,42 @@ def checkColumns(group):
         else:
             return 0
     score = getScore(validColumns)
+    def getAlternates(group):
+        groupCopy = group[:]
+        for rowIdx, row in enumerate(groupCopy):
+            for colIdx in range(len(row)):
+                char = group[rowIdx][colIdx]
+                oppositeChar = '.' if char == '#' else '#'
+                string_list = list(groupCopy[rowIdx])
+                string_list[colIdx] = oppositeChar
+                newString = ''.join(string_list)
+                groupCopy[rowIdx] = newString
+                alternates.append(groupCopy)
+                groupCopy = group[:]
+    if setAlternates:            
+        getAlternates(group)
     return score
 
 # review rows to see if any adjacent rows mirror one another.
 # we can use the same logic as we did for columns if we transpose the group.
 def checkRows(group):
     transposedGroup = [[group[row][col] for row in range(len(group))] for col in range(len(group[0]))]
+    transposedGroup = [''.join(row) for row in transposedGroup]
     targetIndex = checkColumns(transposedGroup)
     return targetIndex * 100
 score = 0
 for groupIndex, group in enumerate(groupings):
     print(f'reviewing group # {groupIndex}')
-    score += checkColumns(group)
-    score += checkRows(group)
+    score += checkColumns(group, True)
+    for altIdx, alternate in enumerate(alternates):
+        if altIdx < 5:
+            print(f'alternate index {altIdx}')
+            print(group)
+            for row in alternate:
+                print((' '*10),row)
+            altResult = (checkColumns(alternate, False))
+            print(altResult)
+    #score += checkRows(group)
     print(f'score is {score}')
 
     
