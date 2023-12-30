73b2feca7bac3bdfd7c6880217671df17d121be1
hey-sk <stephenakeane@gmail.com>
2023-12-30 00:57:47 -0500

    finished day 13

--- 2023/day13/part2.py
+++ 2023/day13/part2.py
@@ -1,6 +1,7 @@
 from functools import cache
 inputPath = "2023/day13/input.txt"
 testPath = "2023/day13/testInput1.txt"
+debugPath = "2023/day13/debug.txt"
 chosenPath = inputPath
 
 def parseInput(path):
@@ -31,6 +32,7 @@ def getAlternates(group):
             alternates.append(groupCopy)
             groupCopy = group[:]
     return alternates
+
 def transpose(group):
     transposedGroup = [[group[row][col] for row in range(len(group))] for col in range(len(group[0]))]
     transposedGroup = [''.join(row) for row in transposedGroup]
@@ -38,7 +40,7 @@ def transpose(group):
 
 groupings = parseInput(chosenPath)
 # review columns to see if any adjacent columns mirror one another.
-def checkColumns(group, lastScore = 0):
+def checkGroup(group, type, lastScore = 0):
     lastIndex = None
     if lastScore > 0:
         lastIndex = lastScore 
@@ -49,80 +51,67 @@ def checkColumns(group, lastScore = 0):
     #accumulate any indexes where the left side and the right side (mirrored) match
     for row in group:
         rowArr = []
-        for colIdx, char in enumerate(row):
+        for colIdx in range(len(row)):
             mirrorLen = colIdx if colIdx < midPoint else len(row) - colIdx
             leftSide, rightSide = (row[colIdx - mirrorLen:colIdx], row[colIdx:colIdx + mirrorLen][::-1])
-            if colIdx > 0 and colIdx != lastIndex:
-                if leftSide == rightSide:
-                    rowArr.append(colIdx)
+            if colIdx > 0:
+                if lastScore == 0 or colIdx != lastIndex:
+                    if leftSide == rightSide:
+                        rowArr.append(colIdx)
+                # we need to exclude the solution to part1 with each iteration. 
+                if colIdx == lastIndex and 1 <= lastScore <= 99 and type == 'row':
+                    if leftSide == rightSide:
+                        rowArr.append(colIdx)
+                if colIdx == lastIndex and  lastScore >= 99 and type == 'col':
+                    if leftSide == rightSide:
+                        rowArr.append(colIdx)
         validColumns.append(rowArr)
     # the common index will be the score.
     def getScore(arr):
+        score = 0
         result = set(arr[0])
         for num in arr[1:]:
             result.intersection_update(num)
         if (len(result)):
-            return (list(result)[0])
-        else:
-            return 0
+            if type == 'col':
+                score = (list(result)[0])
+            else:
+                score = (list(result)[0] * 100)
+        return score
     score = getScore(validColumns)
     return score
 
-# review rows to see if any adjacent rows mirror one another.
-# we can use the same logic as we did for columns if we transpose the group.
-def checkRows(group, lastScore = 0):
-    targetIndex = checkColumns(group, lastScore)
-    return targetIndex * 100
 part1Score = []
 part2Score = []
 
 # review each grouping and check columns and rows for each grouping
 # in order to retrieve the score for each group.
 for groupIndex, group in enumerate(groupings):
-    print(f'reviewing group # {groupIndex}')
-    transposedGroup = transpose(group)
     lastScore = 0
-    if checkColumns(group) != 0:
-        part1Score.append(checkColumns(group))
-        lastScore = checkColumns(group)
-    if checkRows(transposedGroup) != 0:
-        part1Score.append(checkRows(transposedGroup))
-        lastScore = checkRows(transposedGroup)
+    if checkGroup(group, 'col') > 0:
+        part1Score.append(checkGroup(group, 'col'))
+        lastScore = checkGroup(group, 'col')
+    # we can use the same function for checking vertically and horizontally by transposing the group.
+    transposedGroup = transpose(group)
+    if checkGroup(transposedGroup, 'row') > 0:
+        part1Score.append(checkGroup(transposedGroup, 'row'))
+        lastScore = checkGroup(transposedGroup, 'row')
     
     # once the score has been determined generate all of the alternative
     # variations from the orginal grouping. Go through each variation until
     # we have the new score.
     alternates = getAlternates(group)
     for altIdx, alternate in enumerate(alternates):
-        
-        targetRange = (0, 5)
-        betweenCondition = targetRange[0] <= altIdx <= targetRange[1]
-        if betweenCondition:
-            print(f'variation # {altIdx}')
-        if betweenCondition:
-            for row in alternate:
-                print(row)
-            print('-'*20)
         transposedAlternate = transpose(alternate)
-        # alternate scores shouln't be zero and shouldn't be equal to the last score.
-        def isValid(type):
-            if type == 'column':
-                if checkColumns(alternate, lastScore) == 0 or checkColumns(alternate, lastScore) == lastScore:
-                    return False
-                return True
-            if type == 'row':
-                if checkRows(transposedAlternate, lastScore) == 0 or checkRows(transposedAlternate, lastScore) == lastScore:
-                    return False
-                return True
-        if isValid('column'):
-            part2Score.append(checkColumns(alternate, lastScore))
+        colScore = checkGroup(alternate, 'col', lastScore)    
+        if colScore > 0:
+            part2Score.append(colScore)
             break
-        if isValid('row'):
-            part2Score.append(checkRows(transposedAlternate, lastScore))
+        rowScore = checkGroup(transposedAlternate, 'row', lastScore)
+        if rowScore > 0:
+            part2Score.append(rowScore)
             break
 
-    print(f'part 1 score is {sum(part1Score)}')
-    print('len', len(part1Score), part1Score)
-    print(f'part 2 score is {sum(part2Score)}')
-    print('len', len(part2Score), part2Score)
+print(f'part 1 score is {sum(part1Score)}')
+print(f'part 2 score is {sum(part2Score)}')
     
