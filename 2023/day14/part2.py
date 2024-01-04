56a657d8a15e72d9f73613598735495e52c92424
hey-sk <stephenakeane@gmail.com>
2024-01-03 20:09:30 -0500

    solved for part 2

--- 2023/day14/part2.py
+++ 2023/day14/part2.py
@@ -1,90 +1,108 @@
 import time
+from functools import cache
+from multiprocessing import Process
 testPath = '2023/day14/testInput1.txt'
 inputPath = '2023/day14/input.txt'
-chosenPath = testPath
+chosenPath = inputPath
 
+if __name__ == "__main__":
+    def parseInput(path):
+        with open(path, 'r') as file:
+            return [tuple(line.strip()) for line in file]
 
-def parseInput(path):
-    result = []
-    file = open(path, 'r')
-    for line in file:
-        line = line.replace('\n','')
-        result.append([x for x in line])
-    return result
-
-#transpose rows and columns
-def transpose(arr):
-    return [[arr[row][col] for row in range(len(arr))] for col in range(len(arr[0]))]
-
-def tilt(arr, dir):
-    if dir == 'north' or dir == 'south':
-        arr = transpose(arr)
-    newArr = []
-    for row in arr:
-        str = ''
-        subArr = []
-        for colNum, char in enumerate(row):
-            if char != '#':
-                str += char
-            if char == '#':
-                subArr.append(str)
-                subArr.append('#')
-                str = ''
-            if colNum == len(row) - 1:
-                if len(str) > 0:
+    #transpose rows and columns
+    def transpose(arr):
+        return ([arr[row][col] for row in range(len(arr))] for col in range(len(arr[0])))
+    
+    def visualize(arr):
+        for row in arr:
+            print(''.join(row))
+        
+    @cache
+    def tilt (arr, dir):
+        if dir == 'north' or dir == 'south':
+            arr = transpose(arr)
+        newArr = []
+        for row in arr:
+            str = ''
+            subArr = []
+            for colNum, char in enumerate(row):
+                if char != '#' and colNum != len(row) - 1:
+                    str += char
+                elif char == '#':
                     subArr.append(str)
-                subArr = tuple(x for x in subArr if x != '')
-                newArr.append(subArr)
+                    subArr.append('#')
+                    str = ''
+                else:
+                    str += char
+                    subArr.append(str)
+                subArr = [x for x in subArr if x != '']
+                if dir in ('north','west'):
+                    sortedRow = tuple(tuple(sorted(val, reverse=True)) if val.__contains__('O') else val for val in subArr)
+                if dir in ('south', 'east'):
+                    sortedRow = tuple(tuple(sorted(val)) if val.__contains__('O') else val for val in subArr)
+                sortedRow = tuple(''.join(val) for val in sortedRow)
+            newArr.append(tuple(x for x in ''.join(sortedRow)))
+        if dir == 'north' or dir == 'south':
+            return transpose(newArr)
+        return(tuple(newArr))
+
+    @cache
+    def spinCycle(input):
+        result = input
+        directions = ('north', 'west', 'south', 'east')
+        for dir in directions:
+            result = tilt(result, dir)
+        return result
+
+    def getScore(arr):
+        rowNum = len(arr)
+        score = 0
+        for row in arr:
+            letterOs = [char for char in row if char == 'O']
+            score += rowNum * len(letterOs)
+            rowNum -= 1
+        return(score)
+
+    startTime = time.time()
+
+    input = tuple(parseInput(chosenPath))
     
-    for rowNum, row in enumerate(newArr):
-        if dir == 'north' or dir == 'west':
-            sortedRow = [tuple(sorted(val, reverse=True)) for val in row]
-        if dir == 'south' or dir == 'east':
-                sortedRow = [tuple(sorted(val)) for val in row]
-        sortedRow = [''.join(val) for val in sortedRow]
-        newArr[rowNum] = [x for x in ''.join(sortedRow)]
-    if dir == 'north' or dir == 'south':
-        return transpose(newArr)
-    else:
-        return(newArr)
+    def detectCycle(input):
+        def stringify(grid):
+            return ''.join([''.join(row) for row in input])
+        GRIDS = dict()
+        i = 1
+        while True:
+            input = spinCycle(input)
+            hash = stringify(input)
+
+            if hash in GRIDS:
+                cycleLen = i - GRIDS[hash]
+                return (input, i, cycleLen)
+            else: 
+                GRIDS[hash] = i
+                i += 1
     
-def visualize(arr):
-    for row in arr:
-        print(''.join(row))
+    grid, stablePoint, cyclen = detectCycle(input)
+    print(stablePoint, cyclen)
 
-def spinCycle(input):
-    result = input[:]
-    result = tilt(result, 'north')
-    result = tilt(result, 'west')
-    result = tilt(result, 'south')
-    result = tilt(result, 'east')
-    return result
+    num =   1000000000
+    n = 0
+    storedVal = 0
 
+    while n < num:
+        numberofCycles = (num - stablePoint) // cyclen
+        if n < stablePoint:
+            input = (spinCycle(input))
+            n += 1
+        if n == stablePoint:
+            print(cyclen * numberofCycles)
+            n += (cyclen * numberofCycles)
+        if n > stablePoint:
+            input = (spinCycle(input))
+            n += 1
 
-def getScore(arr):
-    rowNum = len(arr)
-    score = 0
-    for row in arr:
-        letterOs = [char for char in row if char == 'O']
-        score += rowNum * len(letterOs)
-        rowNum -= 1
-    return(score)
-
-startTime = time.time()
-
-input = parseInput(chosenPath)
-results = [input]
-scores = []
-num = 10
-print(num)
-for n in range(num):
-    results.append(spinCycle(tuple(results[-1])))
-    scores.append(getScore(results[-1]))
-
-endTime = time.time()
-
-visualize(results[-1])
-print(getScore(results[-1]))
-print(f'elapsed time: {round(endTime - startTime, 2)}')
-for i, score in enumerate(scores):
-    print(i, score)
+    endTime = time.time()
+    print(f'final score = {getScore(input)}')
+    print(f'elapsed time: {round(endTime - startTime, 2)}')
