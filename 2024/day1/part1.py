601094814eadb6e814469076c37191db824e2b15
hey-sk <stephenakeane@gmail.com>
2024-12-01 13:09:58 -0500

    solved for day1 part 1

---
+++ 2024/day1/part1.py
@@ -0,0 +1,41 @@
+inputPath = "./input.txt"
+testPath = "./testInput.txt"
+chosenPath = inputPath
+
+#fn to get the column of numbers from the file
+def getColumn(arrayType, path):
+    def splitInt (arrayType):
+        if arrayType == 'left':
+            return 0
+        else: 
+            return 1
+    file = open(path, "r")
+    result =  []
+    for line in file.readlines():
+        number = int(line.split()[splitInt(arrayType)])
+        result.append(number)
+    return result
+
+# sort both arrays
+rightArr = sorted(getColumn('right', chosenPath))
+leftArr = sorted(getColumn('left', chosenPath))
+
+#fn to get the difference between the two arrays
+def getDifference(leftArr, rightArr):
+    result = []
+    for i in range(len(leftArr)):
+        if leftArr[i] > rightArr[i]:
+            result.append(leftArr[i] - rightArr[i])
+        else:
+            result.append(rightArr[i] - leftArr[i])
+    return result
+
+print (sum(getDifference(leftArr, rightArr)))
+
+
+
+        
+        
+
+
+
